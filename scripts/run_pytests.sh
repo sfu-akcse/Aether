57515d22020a769ef6e385d780f3373a72a71554
Yujin Song <yshinny11@gmail.com>
2026-04-03 17:26:03 -0700

    modifing run_pytests.sh (#15)

--- scripts/run_pytests.sh
+++ scripts/run_pytests.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-set -e
-python -m pytest
+echo "------------------------------------------"
+echo "   Running Aether Pytest Test Suite...   "
+echo "------------------------------------------"
+
+python3 -m pytest -v tests/
+
+RESULT=$?
+
+if [ $RESULT -eq 0 ]; then
+    echo "!!All tests have passed!!"
+fi
+
+exit $RESULT
