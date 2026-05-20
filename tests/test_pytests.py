5d343c74f13df992266db9cbe70e548da13b186e
Yujin Song <ysa284@sfu.ca>
2026-05-20 02:08:53 -0700

    setting pytest (#30)

--- tests/test_pytests.py
+++ tests/test_pytests.py
@@ -1,3 +1,7 @@
+import numpy as np
+from unittest.mock import MagicMock
+
+# sample
 def add(a,b):
   return a+b
 
@@ -9,3 +13,22 @@ def test_pytest_add_edge_case():
 
 def test_pytest_add_negative():
     assert add(-1, -1) == -2  
+
+# XY Coordinate Detection
+# environment
+def make_landmark(x,y):
+    landmark = MagicMock()
+    landmark.x = x
+    landmark.y = y
+    return landmark
+
+# get coordinate data of "hands"
+# def make_mediapipe_result(hands):
+
+
+
+# pytests
+# 1.
+# 2.
+# 3.
+# 4.
