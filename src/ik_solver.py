3375700cb9c40db4d6a2db48573a537ea2b0f809
joshuakim17 <joshuakim1304@gmail.com>
2026-07-09 16:57:46 -0700

    finish initial 2D inverse kinematics solver
    implemented basic 2D IK solver in Python

--- src/ik_solver.py
+++ src/ik_solver.py
@@ -1,9 +1,16 @@
-# input: x,y,L1,L2
-# output: reachable, theta1, theta2
+# 2D Inverse Kinematics Solver
+# Coord plane: z-y 
+
+# input: 
+#   target position: z,y 
+#   link lengths: L1,L2
+# output:
+#   reachability
+#   theoretical shoulder/elbow angles
 
 import math
 
-def solve_ik(z, y, L1, L2, elbow_up=True):
+def solve_ik(z, y, L1, L2, elbow_up, debug):
     """
     Solves 2-D inverse kinematics
 
@@ -13,14 +20,14 @@ def solve_ik(z, y, L1, L2, elbow_up=True):
     L2 = elbow-to-wrist/end-effector link length
     """
 
-    # TODO 1: calculate r
-    r = None
+    # calculate r
+    r = math.sqrt(z**2 + y**2)
 
-    # TODO 2: check reachability
-    min_reach = None
-    max_reach = None
+    # check reachability
+    min_reach = abs(L1 - L2)
+    max_reach = L1 + L2
 
-    if False:  # replace this condition
+    if (r < min_reach) or (r > max_reach):  # if too far / too close
         return {
             "reachable": False,
             "theta1_rad": None,
@@ -29,33 +36,47 @@ def solve_ik(z, y, L1, L2, elbow_up=True):
             "theta2_deg": None,
         }
 
-    # TODO 3: calculate cos(theta2)
-    cos_theta2 = None
+    # calculate cos(theta2)
+    cos_theta2 = (r**2 - L1**2 - L2**2) / (2 * L1 * L2)
 
-    # TODO 4: clamp cos_theta2 between -1 and 1
-    cos_theta2 = None
+    # clamp cos_theta2 between -1 and 1
+    cos_theta2 = max(-1, min(1,cos_theta2))
+    # we do this b/c of tiny floating-point errors
+    # EX. if cos_theta = 1.0000001, would break math.acos()
 
-    # TODO 5: calculate theta2 magnitude using acos
-    theta2_mag = None
+    # calculate theta2 magnitude using acos
+    theta2_mag = math.acos(cos_theta2)
 
-    # TODO 6: choose elbow-up or elbow-down
+    # choose elbow-up or elbow-down
     if elbow_up:
-        theta2 = None
+        theta2 = -theta2_mag
     else:
-        theta2 = None
+        theta2 = theta2_mag
 
-    # TODO 7: calculate phi
-    phi = None
+    # calculate phi
+    # use atan2 to calculate correct angle w.r.t correct quadrant
+    # **** math.atan2(vertical, horizontal) ****
+    phi = math.atan2(y,z)
 
-    # TODO 8: calculate offset angle a
-    a = None
+    # calculate offset angle a
+    # arg1: perpendicular component (the up/down part)
+    # arg2: forward component (forward/backward)
+    a = math.atan2((L2 * math.sin(theta2)), (L1 + L2 * math.cos(theta2)))
 
-    # TODO 9: calculate theta1
-    theta1 = None
+    # calculate theta1
+    theta1 = phi - a
 
-    # TODO 10: convert both to degrees
-    theta1_deg = None
-    theta2_deg = None
+    # convert both to degrees
+    theta1_deg = math.degrees(theta1)
+    theta2_deg = math.degrees(theta2)
+    
+    if debug:
+        print("r:", r)
+        print("cos_theta2:", cos_theta2)
+        print("theta2_deg:", math.degrees(theta2))
+        print("phi_deg:", math.degrees(phi))
+        print("a_deg:", math.degrees(a))
+        print("theta1_deg:", theta1_deg)
 
     return {
         "reachable": True,
@@ -65,6 +86,12 @@ def solve_ik(z, y, L1, L2, elbow_up=True):
         "theta2_deg": theta2_deg,
     }
 
-# test values (below)
-result = solve_ik(z=10,y=10,L1=10,L2=10,elbow_up=True)
+## TESTING VALUES (manual input, for now)
+z = 9
+y = 10
+L1 = L2 = 10
+elbow_up = True
+debug = True # turn debug mode on/off, allowing you to see print statements w/values
+
+result = solve_ik(z,y,L1,L2,elbow_up,debug)
 print(result)
